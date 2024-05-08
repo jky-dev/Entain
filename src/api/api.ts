e368d77e8bb7a5a36015a2c39179727efee3616c
Jacky Liang <liangj97@gmail.com>
2024-05-08 13:10:25 -0400

    comment

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,9 @@ import { RacesResponse } from '../types/types'
 import fixture from './../fixtures/apiFixture.json'
 
 export const getRaces = async (): Promise<RacesResponse> => {
+  // assuming that the next 50 races contain at least 5 races in each category
+  // if not, we could use pagination/fetch more results to iteratively find more races
+  // if a category did not contain at least 5
   const response = await fetch(
     'https://api.neds.com.au/rest/v1/racing/?method=nextraces&count=50',
     {
